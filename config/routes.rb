fda7be691d30f1099cf24c8d0fee274849ba7813
hasanafzal8485 <hasanafzal8485@gmail.com>
2022-08-27 19:44:37 +0500

    add devise vies and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, 
+    :path => '',
+    :path_names => { 
+      :sign_in => "login",
+      :sign_out => "logout",
+      :sign_up => "register"
+    },
+    controllers: {
+      sessions: 'auth/users/sessions',
+      confirmations: 'auth/users/confirmations',
+      passwords: 'auth/users/passwords',
+      unlocks: 'auth/users/unlocks',
+      registrations: 'auth/users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
